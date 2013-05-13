f686239384f46fe9f557a06661501aaadeff1b22
Jiaqing Wang <wjqmichael@gmail.com>
2013-05-13 12:18:22 -0400

    whatever

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 
-cat ~/dotfiles/bashrcs/appnexus >> ~/.bashrc
+ln -s ~/dotfiles/bashrc/appnexus ~/.bashrc
+#cat ~/dotfiles/bashrcs/appnexus >> ~/.bashrc
 echo "source ~/.bashrc" >> ~/.bash_profile
