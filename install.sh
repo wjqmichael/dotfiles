71ed8fc5ac53e5a257f78e1894b94193fbf97b25
Jiaqing Wang <wjqmichael@gmail.com>
2013-05-21 14:01:33 -0400

    snippet

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
+# to avoid the conflicts of snippets setup with ~/dotfiles/vim/snippets
+rm ~/dotfiles/vim/bundle/snipmate/snippets/python.snippets 
 
 ln -s ~/dotfiles/bashrc/appnexus ~/.bashrc
-#cat ~/dotfiles/bashrcs/appnexus >> ~/.bashrc
 echo "source ~/.bashrc" >> ~/.bash_profile
